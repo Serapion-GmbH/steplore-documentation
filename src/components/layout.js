3abd3ab33486db5bb7973a062746de2076219a19
BokiBoki1989 <boris.hrkic@serapion.net>
2025-12-16 11:52:06 +0100

    Update layout.js updated text on hover

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,7 +19,7 @@ const Wrapper = styled('div')`
 
   .sideBarUL .item > a:hover {
     background-color: #CDEB78;
-    color: #fff !important;
+    color: #221F20 !important;
 
     /* background: #F8F8F8 */
   }
